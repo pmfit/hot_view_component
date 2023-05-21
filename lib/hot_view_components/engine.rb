07c18f07ac814d92b699e784a07cf6eb38fe5110
Chris D. MacRae <chrisdmacrae@Chriss-MacBook-Pro.local>
2023-05-21 09:35:11 -0300

    chore: add version management

--- lib/hot_view_components/engine.rb
+++ lib/hot_view_components/engine.rb
@@ -9,14 +9,22 @@ module HotViewComponents
     config.autoload_paths << File.expand_path('./action_view.rb', __dir__)
     config.autoload_paths << File.expand_path('./api.rb', __dir__)
 
-    initializer "hot_view_components.assets" do
-      Rails.application.config.assets.paths << Rails.root.join("app/components")
+    initializer "hot_view_components.assets" do |app|
+      if app.config.respond_to?(:assets)
+        app.config.assets.paths << Rails.root.join("app/components")
+      end
     end
 
-      initializer "hot_view_components.importmap", before: "importmap" do |app|
-        if app.config.methods.include? 'importmap'
-          app.config.importmap.paths << Engine.root.join("config/importmap.rb")
-        end
+    initializer "hot_view_components.importmap", after: "importmap" do |app|
+      if app.config.respond_to?(:importmap)
+        app.config.importmap.paths = [*app.config.importmap.paths, Engine.root.join("config/importmap.rb")]
       end
+    end
+
+    initializer "hot_view_components.importmap.assets", after: "importmap.assets" do |app|
+      if app.config.respond_to?(:assets)
+        app.config.assets.paths << Rails.root.join("app/components")
+      end
+    end
   end
 end
--- lib/install/with_importmap_controllers.rb
+++ lib/install/with_importmap_controllers.rb
@@ -1,4 +1,5 @@
 importmap_entrypoint_path = Rails.root.join("config/importmap.rb")
+application_manifest_path = Rails.root.join("app/assets/config/manifest.js")
 
 def system!(*args)
   system(*args) || abort("\n== Command #{args} failed ==")
@@ -12,6 +13,11 @@ end
 unless File.readlines(importmap_entrypoint_path).grep('pin "@hotwired/stimulus", to: "stimulus.min.js"')
   say "Setting up Stimulus"
   system! "bin/rails stimulus:install"
+end
+
+say "Add component controllers to asset pipeline"
+inject_into_file application_manifest_path, :after => "//= link_tree ../../javascript .js" do
+  "\n//= link_tree ../../components .js"
 end
 
 say "Pin all component controllers"
