e6d18042503e67dce68f0aa2e7c18d61b283f9de
Chris D. MacRae <chrisdmacrae@Chriss-MacBook-Pro.local>
2023-05-22 17:14:19 -0300

    test: basic hot_component generator

--- test/lib/generators/hot_component/hot_component_generator_test.rb
+++ test/lib/generators/hot_component/hot_component_generator_test.rb
@@ -7,10 +7,46 @@ module HotComponent
     destination Rails.root.join("tmp/generators")
     setup :prepare_destination
 
-    # test "generator runs without errors" do
-    #   assert_nothing_raised do
-    #     run_generator ["arguments"]
-    #   end
-    # end
+    test "generator generates a component file" do
+      run_generator ["test"]
+
+      assert_file "app/components/test_component.rb", /TestComponent < ViewComponent::Base/
+    end
+
+    test "generator generates a component stylesheet file" do
+      run_generator ["test"]
+
+      assert_file "app/components/test_component/test_component.css", /.test-component {}/
+    end
+
+    test "generator does not generate a component stylesheet file" do
+      run_generator ["test", "--skip-css"]
+
+      assert_no_file "app/components/test_component/test_component.css"
+    end
+
+    test "generator generates a component view file with a stylesheet binding" do
+      run_generator ["test"]
+
+      assert_file "app/components/test_component/test_component.html.erb", /class="test-component"/
+    end
+
+    test "generator generates a component view file with a controller binding" do
+      run_generator ["test"]
+
+      assert_file "app/components/test_component/test_component.html.erb", /data-controller="test-component"/
+    end
+    
+    test "generator generates a component controller file" do
+      run_generator ["test"]
+
+      assert_file "app/components/test_component/test_component_controller.js", /Connects to data-controller="test-component"/
+    end
+
+    test "generator does not generate a component controller file" do
+      run_generator ["test", "--skip-controller"]
+
+      assert_no_file "app/components/test_component/test_component_controller.js"
+    end
   end
 end
