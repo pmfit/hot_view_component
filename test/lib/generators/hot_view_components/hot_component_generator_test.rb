6a4aae5e685e12b72ca32f58ac96bbd6d4b6ce29
Chris D. MacRae <chrisdmacrae@Chriss-MacBook-Pro.local>
2023-05-22 07:39:36 -0300

    ci: fix gem credentials

---
+++ test/lib/generators/hot_view_components/hot_component_generator_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+require "generators/hot_component/hot_component_generator"
+
+module HotViewComponent
+  class HotComponentGeneratorTest < Rails::Generators::TestCase
+    tests HotComponentGenerator
+    destination Rails.root.join("tmp/generators")
+    setup :prepare_destination
+
+    # test "generator runs without errors" do
+    #   assert_nothing_raised do
+    #     run_generator ["arguments"]
+    #   end
+    # end
+  end
+end
