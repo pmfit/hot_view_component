e62d573e95f4092535f0ab783676c0c19c776bd6
Chris D. MacRae <chrisdmacrae@Chriss-MacBook-Pro.local>
2023-05-28 11:09:19 -0300

    fix version

--- lib/hot_view_component.rb
+++ lib/hot_view_component.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'hot_view_component/version'
 require 'hot_view_component/engine'
 require 'hot_view_component/action_view'
 require 'hot_view_component/props'
